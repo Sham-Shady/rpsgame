d64d29424c5d5d4bbb665764cafed172899e7157
Shamshad Abdullah <abdullas@clarkson.edu>
2021-12-06 13:00:15 -0500

    adding js

--- media.js
+++ media.js
@@ -7,3 +7,26 @@ const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+function computerChoice() {
+    const choices = ['r', 'p', 's'];
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+function win(userInput, compChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    if (userInput === 'r' && compChoice === 's') {
+        result_p.innerHTML = `Computer chosed Scissors. You Win ✅🎉`;
+    }
+    else if (userInput === 'p' && compChoice === 'r') {
+        result_p.innerHTML = `Computer chosed Rock. You Win ✅🎉`;
+    }
+    else if (userInput === 's' && compChoice === 'p') {
+        result_p.innerHTML = `Computer chosed Paper. You Win ✅🎉`;
+    }
+
+    document.getElementById(userInput).classList.add('win');
+    setTimeout(function () { document.getElementById(userInput).classList.remove('win'); }, 350);
+}
