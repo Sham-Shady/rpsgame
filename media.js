0057f97e1760da3d5b5353d90a0d7765c12668f9
Shamshad Abdullah <abdullas@clarkson.edu>
2021-12-06 13:17:27 -0500

    final edits

--- media.js
+++ media.js
@@ -18,15 +18,84 @@ function win(userInput, compChoice) {
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
     if (userInput === 'r' && compChoice === 's') {
-        result_p.innerHTML = `Computer chosed Scissors. You Win ✅🎉`;
+        result_p.innerHTML = `Computer chose Scissors. You Win!`;
     }
     else if (userInput === 'p' && compChoice === 'r') {
-        result_p.innerHTML = `Computer chosed Rock. You Win ✅🎉`;
+        result_p.innerHTML = `Computer chose Rock. You Win!`;
     }
     else if (userInput === 's' && compChoice === 'p') {
-        result_p.innerHTML = `Computer chosed Paper. You Win ✅🎉`;
+        result_p.innerHTML = `Computer chose Paper. You Win!`;
     }
 
     document.getElementById(userInput).classList.add('win');
     setTimeout(function () { document.getElementById(userInput).classList.remove('win'); }, 350);
 }
+
+function Lose(userInput, compChoice) {
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    if (userInput === 'r' && compChoice === 'p') {
+        result_p.innerHTML = `Computer chose Paper. You Lost`;
+    }
+    else if (userInput === 'p' && compChoice === 's') {
+        result_p.innerHTML = `Computer chose Scissors. You Lost`;
+    }
+    else if (userInput === 's' && compChoice === 'r') {
+        result_p.innerHTML = `Computer chose Rock. You Lost`;
+    }
+    document.getElementById(userInput).classList.add('lose');
+    setTimeout(function () { document.getElementById(userInput).classList.remove('lose'); }, 350);
+
+}
+
+function Draw(userInput, compChoice) {
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    if (userInput === 'r' && compChoice === 'r') {
+        result_p.innerHTML = `Computer chose Rock . It's a Draw.`;
+    }
+    else if (userInput === 'p' && compChoice === 'p') {
+        result_p.innerHTML = `Computer chose Paper. It's a Draw.`;
+    }
+    else if (userInput === 's' && compChoice === 's') {
+        result_p.innerHTML = `Computer chose Scissors. It's a Draw.`;
+    }
+    document.getElementById(userInput).classList.add('draw');
+    setTimeout(function () { document.getElementById(userInput).classList.remove('draw'); }, 350);
+}
+
+
+
+function game(userInput) {
+    const compChoice = computerChoice();
+    const UserChoice = userInput + compChoice;
+    if (UserChoice === "rs" || UserChoice === "pr" || UserChoice === "sp") {
+        win(userInput, compChoice);
+        console.log("Win");
+    }
+    else if (UserChoice === "rp" || UserChoice === "ps" || UserChoice === "sr") {
+        Lose(userInput, compChoice);
+        console.log("Lose");
+    }
+    else if (UserChoice === "rr" || UserChoice === "pp" || UserChoice === "ss") {
+        Draw(userInput, compChoice);
+        console.log("Draw");
+    }
+}
+
+
+
+function main() {
+    rock_div.addEventListener('click', function () {
+        game('r');
+    })
+    paper_div.addEventListener('click', function () {
+        game('p');
+    })
+    scissors_div.addEventListener('click', function () {
+        game('s');
+    })
+}
+
+main();
